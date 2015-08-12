9caeb1cf1474cee9f84ec9ef1892228e2709f7db
esc <evan@teespring.com>
2015-08-12 22:55:27 +0000

    Improve README, update funner application content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,23 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render text: "HELLO there WORLD!"
+    randomizer = Random.new
+    random_int = randomizer.rand(3)
+    
+    reloader = "<script>setTimeout(function() { location.reload(); }, 3000);</script>"
+    
+    if random_int == 0
+      render text: reloader + "Well shit"
+    end
+    if random_int == 1
+      render text: reloader + "This is nice"
+    end
+    if random_int == 2
+      render text: reloader + "<br /><br /> wooooowie!"
+    end
+    if random_int == 3
+      render text: reloader + "Or this maybe..."
+    end
+    # render text: "HELLO there WORLD!" + random_int.to_s
   end
 end
